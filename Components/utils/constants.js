fe2ca442ba3b0c7492c3035f17f8890f2144e622
jparashar <jayant.parashar@cognizant.com>
2020-09-23 20:37:50 +0530

    refactor : updated utils

---
+++ Components/utils/constants.js
@@ -0,0 +1,11 @@
+const CONSTANTS = {
+    // generic texts
+    LABEL_YEAR: 'year',
+    LABEL_SUCCESS_LANDING: 'landings',
+    LABEL_SUCCESS_LAUNCH: 'launch',
+    QUERY_LABEL_YEAR: 'launch_year',
+    QUERY_LABEL_SUCCESS_LANDING: 'land_success',
+    QUERY_LABEL_SUCCESS_LAUNCH: 'launch_success'
+};
+
+export default CONSTANTS;
