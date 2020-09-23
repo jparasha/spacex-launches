1b8501e750ddde2fd4dfac72460f980ed070a331
jparashar <jayant.parashar@cognizant.com>
2020-09-23 20:36:47 +0530

    refactor : route updated

--- Components/atoms/chips/index.js
+++ Components/atoms/chips/index.js
@@ -2,7 +2,6 @@ import React from "react";
 
 export default function Chip(props) {
   const { title = "test", isSelected = false, type = 'year', handleClick } = props || {};
-
   return (
     <div
       className={`chip ${isSelected ? "selected" : ""}`}
