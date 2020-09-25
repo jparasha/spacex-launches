fbd3b958992d225c068edd92b856fe680a0f46e4
jparashar <jayant.parashar@cognizant.com>
2020-09-25 12:33:06 +0530

    feature : added footer comp

---
+++ Components/organisms/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+
+    return (
+        <footer className={"footer"}>
+            <a
+                href="https://github.com/jparasha"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={"link "}
+            >
+                Jayant Parashar
+            </a>
+        </footer>);
+}
