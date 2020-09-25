96b23603715b1d5f878f74d6eb50f4b0fe4f5c0b
jparashar <jayant.parashar@cognizant.com>
2020-09-25 13:14:45 +0530

    refactor : updated footer + seo fixes

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,16 @@
 import Head from "next/head";
 import HomePage from "../Components/organisms/HomePage";
-
+import Footer from '../Components/organisms/Footer';
 export default function Home() {
   return (
     <div className={"container"}>
       <Head>
-        <title>SPACEX LAUNCHES | Demo</title>
+        <title>SPACEX LAUNCHES | Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <HomePage />
-      <footer className={"footer"}>
-        <a
-          href="https://github.com/jparasha"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={"link "}
-        >
-         Jayant Parashar
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
